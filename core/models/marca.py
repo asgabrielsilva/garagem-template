5b3e70a860d02969063c93892f1c6ded94a6f48f
asgabrielsilva <gabrielalmeida290905@gmail.com>
2024-07-05 16:43:50 -0300

    Marca pronto

--- core/models/marca.py
+++ core/models/marca.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Marca(models.Model):
     nome = models.CharField(max_length=50)
-    nacionalidade = models.CharField(max_length=50, null=True)
+    nacionalidade = models.CharField(max_length=50, blank=True, null=True)
 
     def __str__(self):
-        return f"{self.nome.Upper()} ({self.id})"
+        return f"{self.nome.upper()} ({self.id})"
